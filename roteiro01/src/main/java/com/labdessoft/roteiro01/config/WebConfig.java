4152fb3eb361813d6f1c8ef40b775d4597d98c94
unknown <acreisjunio@gmail.com>
2024-06-22 12:41:05 -0300

    corrige config cors

--- roteiro01/src/main/java/com/labdessoft/roteiro01/config/WebConfig.java
+++ roteiro01/src/main/java/com/labdessoft/roteiro01/config/WebConfig.java
@@ -1,6 +1,5 @@
-package main.java.com.labdessoft.roteiro01.config;
+package com.labdessoft.roteiro01.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,12 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @SuppressWarnings("null")
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("*");
     }
 }
